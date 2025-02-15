9f0571a01ab2697bd0df7f2ca5b48a84cf7e128c
J-Pierce <james.pierce@hotmail.co.uk>
2025-02-15 23:25:56 +0000

    Still working on inquirer

--- inquirerPractise.js
+++ inquirerPractise.js
@@ -4,8 +4,55 @@ const inquirer = require('inquirer');
 const pokemonChoices = Object.keys(makePokemon())
 
 function chooseStartingPokemon () {
-    
-    const list = inquirer.prompt([
+    inquirer
+    .prompt([
+        {
+          name: "startingPokemon",
+          type: "checkbox",
+          message: "Choose 6 pokemon to equip to your belt:",
+          loop: false,
+          choices: pokemonChoices,
+        },
+      ])
+    .then((answer) => {
+        if (answer.startingPokemon.length < 6) {
+            console.log("That wasn't enough pokemon, you need to choose 6!");
+            chooseStartingPokemon();
+        }
+        else if (answer.startingPokemon.length > 6) {
+            console.log("That was too many pokemon, you need to choose 6!");
+            chooseStartingPokemon();
+        }
+
+        console.log("Your starting belt now contains: ", answer.startingPokemon);
+        return answer
+    })
+    .then((answer) => {
+        inquirer.prompt([
+            {
+              name: "startingPokemon",
+              type: "checkbox",
+              message: "Choose 6 pokemon to equip to your belt:",
+              loop: false,
+              choices: answer.startingPokemon
+            },
+          ])
+          .then((answer) => {
+            console.log(answer)
+          })
+
+    });
+  };
+
+chooseStartingPokemon()
+
+//  const list = chooseStartingPokemon();
+
+
+
+
+/*
+   const list = inquirer.prompt([
     {
         type: 'input',
         name: 'name',
@@ -33,19 +80,4 @@ function chooseStartingPokemon () {
     })
     
     return list
-};
-
-
-// .then(inquirer.prompt([
-//     {
-//     type: 'chosenPokemon',
-//     name: 'pokemon',
-//     message: 'Which pokemon do you want to send out first?',
-//     choices: answer.startingPokemon,
-//     }])
-// .then((answer) => {
-//     console.log(answer)
-// }))
-
-const list = chooseStartingPokemon();
-
+*/
