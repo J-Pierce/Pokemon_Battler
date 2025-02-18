448f4dedfc35b0e3a1f2dd9eea426b8fc9617cc4
J-Pierce <james.pierce@hotmail.co.uk>
2025-02-18 22:08:00 +0000

    gave opponent full belt functionality

--- game_files/functions/playGame.js
+++ game_files/functions/playGame.js
@@ -11,7 +11,7 @@ const {
 } = require("../classes/indexClasses");
 const { makePokemon } = require("./pokemonCreator");
 
-const question1 = [
+const userBelt = [
   {
     type: "input",
     name: "name",
@@ -27,7 +27,17 @@ const question1 = [
   },
 ];
 
-const question2 = [
+const opponentBelt = [
+  {
+    name: "startingPokemon",
+    type: "checkbox",
+    message: "Choose 6 pokemon to equip to your opponents belt:",
+    loop: false,
+    choices: Object.keys(makePokemon()),
+  },
+];
+
+const userStartingPokemon = [
   {
     type: "list",
     name: "chosenPokemon",
@@ -36,61 +46,82 @@ const question2 = [
   },
 ];
 
-const question3 = [
+const opponentStartingPokemon = [
   {
     type: "list",
     name: "opponentPokemon",
-    message: "Which pokemon would you like your pokemon to battle?",
+    message: "Which pokemon should your opponent send out first to battle?",
     loop: false,
-    choices: Object.keys(makePokemon()),
+    choices: [],
   },
 ];
 
-function chooseStartingPokemon() {
-  return inquirer.prompt(question1).then((data) => {
+function chooseStartingPokemon(question) {
+  return inquirer.prompt(question).then((data) => {
     if (data.startingPokemon.length < 6) {
       console.log("That wasn't enough pokemon, you need to choose 6!");
-      return chooseStartingPokemon();
+      return chooseStartingPokemon(question);
     } else if (data.startingPokemon.length > 6) {
       console.log("That was too many pokemon, you need to choose 6!");
-      return chooseStartingPokemon();
+      return chooseStartingPokemon(question);
     } else {
       return data;
     }
   });
 }
 
-chooseStartingPokemon()
-  .then((data) => {
-    question2[0].choices = data.startingPokemon;
-    const pokemon = data.startingPokemon;
-    const trainer = new Trainer(
-      makePokemon(pokemon[0]),
-      makePokemon(pokemon[1]),
-      makePokemon(pokemon[2]),
-      makePokemon(pokemon[3]),
-      makePokemon(pokemon[4]),
-      makePokemon(pokemon[5]),
-      data.name
-    );
-    return trainer;
+chooseStartingPokemon(userBelt)
+  .then((userChoice) => {
+    return Promise.all([userChoice, chooseStartingPokemon(opponentBelt)]);
   })
-  .then((trainer) => {
-    return Promise.all([{ trainer }, inquirer.prompt(question2)]);
+  .then((data) => {
+    const userPokemon = data[0].startingPokemon;
+    const opponentPokemon = data[1].startingPokemon;
+
+    userStartingPokemon[0].choices = data[0].startingPokemon;
+    opponentStartingPokemon[0].choices = data[1].startingPokemon;
+
+    const trainer = new Trainer(
+      makePokemon(userPokemon[0]),
+      makePokemon(userPokemon[1]),
+      makePokemon(userPokemon[2]),
+      makePokemon(userPokemon[3]),
+      makePokemon(userPokemon[4]),
+      makePokemon(userPokemon[5]),
+      data[0].name
+    );
+
+    const opponent = new Trainer(
+      makePokemon(opponentPokemon[0]),
+      makePokemon(opponentPokemon[1]),
+      makePokemon(opponentPokemon[2]),
+      makePokemon(opponentPokemon[3]),
+      makePokemon(opponentPokemon[4]),
+      makePokemon(opponentPokemon[5]),
+      "Opponent"
+    );
+
+    return { trainer, opponent };
+  })
+  .then((players) => {
+    return Promise.all([players, inquirer.prompt(userStartingPokemon)]);
   })
   .then((trainerData) => {
-    return Promise.all([...trainerData, inquirer.prompt(question3)]);
+    return Promise.all([
+      ...trainerData,
+      inquirer.prompt(opponentStartingPokemon),
+    ]);
   })
   .then((battleData) => {
     const trainer = battleData[0]["trainer"];
+    const opponent = battleData[0]["opponent"];
     const chosenPokemon = battleData[1]["chosenPokemon"];
     const opponentPokemon = battleData[2]["opponentPokemon"];
-    const opponentTrainer = new Trainer(makePokemon(opponentPokemon));
 
     const battle = new Battle(
       trainer,
       chosenPokemon,
-      opponentTrainer,
+      opponent,
       opponentPokemon
     );
 
